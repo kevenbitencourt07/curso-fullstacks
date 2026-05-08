5ab3e9bb1eba6cef41e806cc21c823ac9b3945fa
kevenbitencourt07 <kevenbitencourt123@gmail.com>
2026-05-08 14:33:29 -0300

    alteração

---
+++ git/curso-fullstacks/mini-site/css/js/script.js
@@ -0,0 +1,14 @@
+let valor = 10;
+console.log ("Agora vale: ");
+
+valor = valor + 1
+
+console.log ("Agora vale: ", valor);
+
+valor ++;
+
+console.log ("Agora vale: ", valor);
+
+valor += 10
+
+console.log ("Agora vale: ",  valor);
