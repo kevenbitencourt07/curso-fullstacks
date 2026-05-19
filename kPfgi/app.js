8af989861f66bf969455476adc1a2feca3a80b56
kevenbitencourt07 <kevenbitencourt123@gmail.com>
2026-05-19 15:00:27 -0300

    feat

--- kPfgi/app.js
+++ kPfgi/app.js
@@ -2,13 +2,36 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Variável com os dados
+const alimentos = [
+    {id: 1, nome: "Feijão", preco: 10},
+    {id: 2, nome: "Carne", preco: 50},
+    {id: 3, nome: "Frutas", preco: 70},
+    {id: 4, nome: "Sopas", preco: 40}
+];
+
+const tiposPratos = [
+    {id: 1, nome: "Prato Executivo", preco: 10},
+    {id: 2, nome: "Prato Princioal", preco: 50},
+    {id: 3, nome: "Prato Especial", preco: 70},
+    {id: 4, nome: "Promoção do dia", preco: 40}
+]
+
 // Criar uma função
 // Rota principal
 app.get('/', (req, res) => {
     res.send("Bem vindo ao meu projeto de Alimentos")
-})
+});
+
+// Produtos
+app.get('/produtos', (req, res) => {
+    res.send(alimentos);
+});
+app.get('/cardapio', (req, res) => {
+    res.send(alimentos);
+});
 
 // Executando o sevidor
 app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${port}`);
-})
+    console.log(`Servidor do projeto, rodando em http://localhost:${port}`);
+});
